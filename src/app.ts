c91f92f35bbea6952071b2245913ddc7fae5e6d9
sanjaydambal <sanjaydambal@gmail.com>
2024-05-22 23:46:11 +0530

    design patterns

--- src/app.ts
+++ src/app.ts
@@ -13,13 +13,44 @@ console.log(arr)
 interface obj {
     id:number;
     name:string;
-
+    test:()=>void;
+    test2:()=>void;
 }
 let obj1:obj = {
     id:1,
-    name:"sanjay"
+    name:"sanjay",
+    test : ()=>{console.log("testing")},
+    test2 : ()=>{console.log("testing2")}
 }
 console.log(obj1.name)
+interface Animal {
+    speak: () => void
+  }
+
+  class Dog implements Animal {
+    speak() {
+      console.log('Woof! I am Dog!')
+    }
+  }
+
+  class Cat implements Animal {
+    speak() {
+      console.log('Meoww! I am Cat!')
+    }
+  }
+
+  class AnimalFactory {
+    static createAnimal(type: 'dog' | 'cat'): Animal {
+      if (type === 'dog') {
+        return new Dog()
+      }
+
+      if (type === 'cat') {
+        return new Cat()
+      } else throw new Error()
+    }
+  }
+
 
 function greet(name:string){
 return `hello ${name}`
@@ -28,20 +59,20 @@ console.log(greet("sanjay"))
 
 let description : (string | number) = `Hello am learning the basics of typescript throuh Typescript tutorial and will complete by today 6 pm`;
 console.log(description);
-const pending : boolean = true
-const hasError : boolean = false;
-let result = pending || hasError;
+const pending : boolean = false
+const hasError : boolean = true;
+let result = pending && hasError;
 console.log(result);
 let vacant: {} = {};
 
 console.log(vacant.toString());
-let arr2 :(number | string|boolean)[] = ["Js","Typescript",5,true];
+let arr2 :(number | string|boolean |null)[] = ["Js","Typescript",5,true,null];
 console.log(arr2)
-let skill : [string,number] = ["Js",5]
+let skill : [string,number] = ["Js",8]
 console.log(skill);
-let color,color2 : [number,number,number?]; color= [255,0,255];color2 = [255,0];
+let color,color2 : [number,number,number?]; color= [255,0,255];color2 = [255,0,0];
 console.log(color2)
-enum Month{
+enum Month{ 
     Jan,
     Feb,
     Mar,
@@ -84,6 +115,24 @@ console.log(add("g","o"))
 type MouseEvent = 'click' | 'dblclick' | 'mouseup' | 'mousedown';
 let mouseEvent: MouseEvent;
 mouseEvent = 'click';
+let counter1 = 99;
+let max = 100;
+counter1<max ? counter1++ :counter1 = 1
+console.log(counter1)
 app.listen(3000, () => {
     console.log('listening on port 3000');
 })
+interface SortingStretegy {
+    sort(arr: number[]): number[]
+  }
+
+  class BubbleSort implements SortingStretegy {
+    sort(arr: number[]): number[] {
+      console.log('Using Bubble Sort!')
+      // Implement Here
+      return arr
+    }
+    test(){
+        console.log("tested")
+    }
+  }
